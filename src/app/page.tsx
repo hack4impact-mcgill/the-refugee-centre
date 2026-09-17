198e832938d373cebfd1fe8493d92285f0f27c18
tran-ethan <tran.ethan@hotmail.com>
2026-09-17 10:05:38 -0400

    try version 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         />
         <div>
-          Testing version 2 test on main
+          Testing version 3 test on main
         </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
